3a67e3b70db43ec0824e0173e22bf65dd98b6e3e
Rolly <recabarrolly@gmail.com>
2024-11-18 15:39:48 +0800

    test

--- admin/login.php
+++ admin/login.php
@@ -69,7 +69,7 @@
         success: function(response){
             if(response.status === 'success'){
                 // Redirect to dashboard or whatever page after successful login
-                window.location.href = 'dashboard.php';
+                window.location.href = 'home.php';
             } else if(response.status === 'incorrect'){
                 alert('Incorrect username or password');
             } else if(response.status === 'locked'){

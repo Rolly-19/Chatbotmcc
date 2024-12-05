7ac04850230847bb64d684bd8c08a9ca39db0cfa
Rolly <recabarrolly@gmail.com>
2024-12-05 17:17:02 +0800

    test

--- admin/login.php
+++ admin/login.php
@@ -16,10 +16,10 @@
     <div class="card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form id="login-frm" action="login.php" method="post">
+      <form id="login-frm" action="" method="post">
   <input type="hidden" id="recaptchaToken" name="recaptchaToken">
   <div class="input-group mb-3">
-    <input type="email" class="form-control" name="username" placeholder="Email" required>
+    <input type="email" class="form-control" name="username" placeholder="Email">
     <div class="input-group-append">
       <div class="input-group-text">
         <span class="fas fa-user"></span>
@@ -27,15 +27,37 @@
     </div>
   </div>
   <div class="input-group mb-3">
-    <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+    <input type="password" class="form-control" name="password" id="password" placeholder="Password">
     <div class="input-group-append">
       <div class="input-group-text">
         <span id="toggle-password" class="fas fa-eye"></span>
       </div>
     </div>
   </div>
-  <button type="submit" class="btn btn-primary">Sign In</button>
+  <div class="row">
+    <div class="col-12">
+      <a href="<?php echo base_url ?>admin/3ways" class="text-left">Forgot Password?</a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-6">
+      <a href="<?php echo base_url ?>" class="btn btn-link">Go to Website</a>
+    </div>
+    <div class="col-6 text-right">
+      <button type="submit" class="btn btn-primary">Sign In</button>
+    </div>
+  </div>
 </form>
+    </div>
+  </div>
+</div>
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
 
 <script src="https://www.google.com/recaptcha/api.js?render=6LcT_pIqAAAAAIVwkx4EyPagkk-w-c0RhI-P-FLW"></script>
 <script>
@@ -55,6 +77,23 @@
             document.getElementById("login-frm").submit();
         });
     }
+  });
+</script>
+<script>
+  $(document).ready(function(){
+    end_loader();
+    
+    $('#toggle-password').click(function(){
+      var passwordField = $('#password');
+      var passwordFieldType = passwordField.attr('type');
+      if (passwordFieldType === 'password') {
+        passwordField.attr('type', 'text');
+        $(this).removeClass('fa-eye').addClass('fa-eye-slash');
+      } else {
+        passwordField.attr('type', 'password');
+        $(this).removeClass('fa-eye-slash').addClass('fa-eye');
+      }
+    });
   });
 </script>
 

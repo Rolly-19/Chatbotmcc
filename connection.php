dcfc6d91ac5ff93dbcf4480892fc98f84717f77b
Rolly <recabarrolly@gmail.com>
2024-12-08 17:20:01 +0800

    test

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "u510162695_chatbot_db";
-$password = "1Chatbot_db";
-$dbname = "u510162695_chatbot_db";
+$username = "your_username"; // Replace with your DB username
+$password = "your_password"; // Replace with your DB password
+$dbname = "chatbot_db"; // Replace with your DB name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,11 +12,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// SQL to fetch data from the `user_logins` table
-$sql = "SELECT ul.id, ul.user_id, u.firstname, u.lastname, ul.time_in, ul.time_out
-        FROM user_logins ul
-        INNER JOIN users u ON ul.user_id = u.id
-        ORDER BY ul.time_in DESC";
+// Fetch all data from the `users` table
+$sql = "SELECT * FROM users";
 $result = $conn->query($sql);
 
 // Display the table
@@ -24,25 +21,33 @@ if ($result->num_rows > 0) {
     echo "<table border='1' cellpadding='10' cellspacing='0'>";
     echo "<tr>
             <th>ID</th>
-            <th>User ID</th>
             <th>First Name</th>
             <th>Last Name</th>
-            <th>Time In</th>
-            <th>Time Out</th>
+            <th>Username</th>
+            <th>Password</th>
+            <th>Avatar</th>
+            <th>Last Login</th>
+            <th>Date Added</th>
+            <th>Date Updated</th>
+            <th>OTP</th>
           </tr>";
     while ($row = $result->fetch_assoc()) {
         echo "<tr>
                 <td>{$row['id']}</td>
-                <td>{$row['user_id']}</td>
                 <td>{$row['firstname']}</td>
                 <td>{$row['lastname']}</td>
-                <td>{$row['time_in']}</td>
-                <td>" . ($row['time_out'] ?? "N/A") . "</td>
+                <td>{$row['username']}</td>
+                <td>{$row['password']}</td>
+                <td>{$row['avatar']}</td>
+                <td>{$row['last_login']}</td>
+                <td>{$row['date_added']}</td>
+                <td>{$row['date_updated']}</td>
+                <td>{$row['OTP']}</td>
               </tr>";
     }
     echo "</table>";
 } else {
-    echo "No records found in the `user_logins` table.";
+    echo "No users found.";
 }
 
 // Close the connection

99ebf29264fbaaf2b0238f66ab97d13789f4c566
Rolly <recabarrolly@gmail.com>
2024-12-11 08:41:50 +0800

    test

--- admin/login.php
+++ admin/login.php
@@ -17,37 +17,46 @@
       <p class="login-box-msg">Sign in to start your session</p>
 
       <form id="login-frm" action="" method="post">
-  <input type="hidden" id="recaptchaToken" name="recaptchaToken">
-  <div class="input-group mb-3">
-    <input type="email" class="form-control" name="username" placeholder="Email">
-    <div class="input-group-append">
-      <div class="input-group-text">
-        <span class="fas fa-user"></span>
-      </div>
-    </div>
-  </div>
-  <div class="input-group mb-3">
-    <input type="password" class="form-control" name="password" id="password" placeholder="Password">
-    <div class="input-group-append">
-      <div class="input-group-text">
-        <span id="toggle-password" class="fas fa-eye"></span>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-12">
-      <a href="<?php echo base_url ?>admin/3ways" class="text-left">Forgot Password?</a>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-6">
-      <a href="<?php echo base_url ?>" class="btn btn-link">Go to Website</a>
-    </div>
-    <div class="col-6 text-right">
-      <button type="submit" class="btn btn-primary">Sign In</button>
-    </div>
-  </div>
-</form>
+        <input type="hidden" id="recaptchaToken" name="recaptchaToken">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" name="username" placeholder="Email" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span id="toggle-password" class="fas fa-eye"></span>
+            </div>
+          </div>
+        </div>
+
+        <!-- Terms and Conditions Checkbox -->
+        <div class="form-check mb-3">
+          <input type="checkbox" class="form-check-input" id="terms_conditions" disabled>
+          <label for="terms_conditions" class="form-check-label">
+            I agree to the <a href="#" data-bs-toggle="modal" data-bs-target="#termsModal">Terms and Conditions</a>
+          </label>
+        </div>
+
+        <div class="row">
+          <div class="col-12">
+            <a href="<?php echo base_url ?>admin/3ways" class="text-left">Forgot Password?</a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-6">
+            <a href="<?php echo base_url ?>" class="btn btn-link">Go to Website</a>
+          </div>
+          <div class="col-6 text-right">
+            <button type="submit" class="btn btn-primary" id="login-btn" disabled>Sign In</button>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </div>
@@ -83,38 +92,12 @@
   </div>
 </div>
 
-<!-- jQuery -->
+<!-- Include JavaScript Libraries -->
 <script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
-<!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Bootstrap Bundle (includes Popper and JavaScript) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-
 <script src="https://www.google.com/recaptcha/api.js?render=6LcT_pIqAAAAAIVwkx4EyPagkk-w-c0RhI-P-FLW"></script>
-<script>
-  grecaptcha.ready(function() {
-    grecaptcha.execute('6LcT_pIqAAAAAIVwkx4EyPagkk-w-c0RhI-P-FLW', { action: 'login' }).then(function(token) {
-      document.getElementById('recaptchaToken').value = token;
-    });
-  });
 
-  // document.getElementById("login-frm").addEventListener("submit", function(event) {
-  //   const recaptchaTokenField = document.getElementById('recaptchaToken');
-
-  //   if (!recaptchaTokenField.value) {
-  //       event.preventDefault();
-  //       grecaptcha.execute('6LcT_pIqAAAAAIVwkx4EyPagkk-w-c0RhI-P-FLW', { action: 'login' }).then(function(token) {
-  //           recaptchaTokenField.value = token;
-  //           document.getElementById("login-frm").submit();
-  //       });
-  //   }
-  // });
-</script>
 <script>
   $(document).ready(function(){
     // Show/hide password functionality
@@ -132,26 +115,21 @@
 
     // Enable checkbox and Sign-In button after accepting terms
     $('#acceptTerms').click(function () {
-      // Enable and check the checkbox
       $('#terms_conditions').prop('disabled', false).prop('checked', true);
-      // Enable the login button
       $('#login-btn').prop('disabled', false);
     });
 
-    // Ensure checkbox styles update properly when toggled
-    $('#terms_conditions').on('change', function () {
-      if (this.checked) {
-        this.style.accentColor = 'dodgerblue';
-      } else {
-        this.style.accentColor = 'gray';
-      }
-    });
-
-    // Debugging tip: Ensure the loader ends properly
+    // Ensure the loader ends properly
     end_loader();
   });
-</script>
 
+  // ReCAPTCHA functionality
+  grecaptcha.ready(function() {
+    grecaptcha.execute('6LcT_pIqAAAAAIVwkx4EyPagkk-w-c0RhI-P-FLW', { action: 'login' }).then(function(token) {
+      document.getElementById('recaptchaToken').value = token;
+    });
+  });
+</script>
 
 <style>
   .login-logo {

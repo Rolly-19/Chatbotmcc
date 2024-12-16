73363ae4d7c6def13b2f70ed61bc411dd4fef183
Rolly <recabarrolly@gmail.com>
2024-12-16 23:04:03 +0800

    test

--- admin/home.php
+++ admin/home.php
@@ -66,39 +66,7 @@
 
 <script>
   $(function() {
-    // Bar Chart Data (Top 10 Frequent Questions)
-    var barChartData = {
-      labels  : ['<?php echo implode('\',\'',$label) ?>'],
-      datasets: [{
-        label               : 'Frequent Asks',
-        backgroundColor     : [
-          'rgba(60,141,188,0.9)',
-          'rgba(210, 214, 222, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 205, 86, 1)',
-          'rgba(231, 233, 237, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 99, 132, 1)',
-          'rgba(201, 203, 207, 1)'
-        ],
-        borderColor         : [
-          'rgba(60,141,188,1)',
-          'rgba(210, 214, 222, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 205, 86, 1)',
-          'rgba(231, 233, 237, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 99, 132, 1)',
-          'rgba(201, 203, 207, 1)'
-        ],
-        data                : [<?php echo implode(',',$data) ?>]
-      }]
-    };
-
+   
     // Bar Chart Options
     var barChartOptions = {
       maintainAspectRatio : false,
